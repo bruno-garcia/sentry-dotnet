c9f42f05dc417fd06d5eb110ca48557432625698
Bruno Garcia <bruno@brunogarcia.com>
2020-03-28 14:57:19 -0300

    assembly signing nightmare

---
+++ samples/Sentry.Samples.Xamarin.Android/MainActivity.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using AndroidX.AppCompat.App;
+using Google.Android.Material.FloatingActionButton;
+using Google.Android.Material.Snackbar;
+using Sentry.Xamarin;
+using Sentry;
+
+namespace Sentry.Samples.Xamarin.Android
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            SentryXamarin.Init(o =>
+            {
+                o.Dsn = new Dsn("https://5fd7a6cda8444965bade9ccfd3df9882@sentry.io/1188141");
+            });
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            AndroidX.AppCompat.Widget.Toolbar toolbar = FindViewById<AndroidX.AppCompat.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            var fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab.Click += FabOnClick;
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void FabOnClick(object sender, EventArgs eventArgs)
+        {
+            var view = (View)sender;
+            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+                .SetAction("Action", (View.IOnClickListener)null).Show();
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] global::Android.Content.PM.Permission[] grantResults)
+        {
+            global::Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ src/Sentry.Xamarin/SentryXamarin.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Android.OS;
+using Android.Systems;
+using Sentry;
+
+namespace Sentry.Xamarin
+{
+    public static class SentryXamarin
+    {
+        public static void Init(Action<XamarinOptions> configureOptions)
+        {
+            var options = new XamarinOptions();
+            configureOptions?.Invoke(options);
+            SentrySdk.Init(options);
+
+            // TODO: This should be part of a package: Sentry.Xamarin.Android
+            SentrySdk.ConfigureScope(s =>
+            {
+#if !ANDROID
+#pragma warning disable CS0618 // Type or member is obsolete
+                var friendlyName = $"Android:{Build.Manufacturer}-{Build.CpuAbi}-{Build.Model}";
+                StructUtsname uname = null;
+                if (Build.VERSION.SdkInt > BuildVersionCodes.Lollipop)
+                {
+                    try
+                    {
+                        uname = Os.Uname();
+                        friendlyName += $"-kernel-{uname.Release}";
+                        s.Contexts["uname"] = new
+                        {
+                            uname.Machine,
+                            uname.Nodename,
+                            uname.Release,
+                            uname.Sysname,
+                            uname.Version
+                        };
+                    }
+                    catch
+                    {
+                        // android.runtime.JavaProxyThrowable: System.NotSupportedException: Could not activate JNI Handle 0x7ed00025 (key_handle 0x4192edf8) of Java type 'md5eb7159ad9d3514ee216d1abd14b6d16a/MainActivity' as managed type 'SymbolCollector.Android.MainActivity'. --->
+                        // Java.Lang.NoClassDefFoundError: android/system/Os ---> Java.Lang.ClassNotFoundException: Didn't find class "android.system.Os" on path: DexPathList[[zip file "/data/app/SymbolCollector.Android.SymbolCollector.Android-1.apk"],nativeLibraryDirectories=[/data/app-lib/SymbolCollector.Android.SymbolCollector.Android-1, /vendor/lib, /system/lib]]
+                    }
+                }
+
+                s.User.Id = Build.Id;
+                s.Contexts.Device.Architecture = Build.CpuAbi;
+                s.Contexts.Device.Brand = Build.Brand;
+                s.Contexts.Device.Manufacturer = Build.Manufacturer;
+                s.Contexts.Device.Model = Build.Model;
+
+                s.Contexts.OperatingSystem.Name = "Android";
+                s.Contexts.OperatingSystem.KernelVersion = uname?.Release;
+                s.Contexts.OperatingSystem.Version = Build.VERSION.SdkInt.ToString();
+
+                s.SetTag("API", ((int)Build.VERSION.SdkInt).ToString());
+                s.SetTag("host", Build.Host);
+                s.SetTag("device", Build.Device);
+                s.SetTag("product", Build.Product);
+                s.SetTag("cpu-abi", Build.CpuAbi);
+                s.SetTag("fingerprint", Build.Fingerprint);
+
+                if (!string.IsNullOrEmpty(Build.CpuAbi2))
+                {
+                    s.SetTag("cpu-abi2", Build.CpuAbi2);
+                }
+#pragma warning restore CS0618 // Type or member is obsolete
+#endif
+            });
+        }
+    }
+
+    public class XamarinOptions : SentryOptions
+    {
+        public XamarinOptions()
+        {
+            this.AddInAppExclude("Mono");
+        }
+    }
+}
